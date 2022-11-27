314b4a93393102d7687f3d5ec5ba5bc788dec962
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2022-11-27 16:40:54 +0200

    Adaugarea metodei make storage si restructurarea claselor

--- HttpServer/QuestionABCD.h
+++ HttpServer/QuestionABCD.h
@@ -1,1 +1,13 @@
 #pragma once
+struct QuestionABCD
+{
+	int id;
+	std::string m_question;
+	std::string m_answerA;
+	std::string m_answerB;
+	std::string m_answerC;
+	std::string m_answerD;
+	std::string m_correctAnswer;
+	QuestionABCD() = default;
+	QuestionABCD(std::string question, std::string answerA, std::string answerB, std::string answerC, std::string answerD, std::string correctAnswer);
+};
--- HttpServer/QuestionNumeric.h
+++ HttpServer/QuestionNumeric.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <string>
+struct QuestionNumeric
+{
+	int id;
+	std::string m_question;
+	float m_correctAnswer;
+	QuestionNumeric() = default;
+	QuestionNumeric(std::string question, float correctAnswer);
+};
