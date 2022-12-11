f81dc42869c1829cd08731c20ea1622ec74250a0
tudorp9k <tudorp9k@gmail.com>
2022-12-11 17:17:02 +0200

    Am implementat functionalitatea butonului pentru noua pagina

--- Client1/MainPage.cpp
+++ Client1/MainPage.cpp
@@ -1,10 +1,14 @@
 #include "MainPage.h"
-
-MainPage::MainPage(QWidget *parent)
+MainPage::MainPage(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 }
-
+void MainPage::on_Cont_clicked()
+{
+	accountPage = new AccountPage(this);
+	accountPage->resize(800, 600);
+	accountPage->show();
+}
 MainPage::~MainPage()
 {}
