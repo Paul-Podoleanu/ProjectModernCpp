eb54bfb0b977c061bb4a73252611f95a5e494169
tudorp9k <tudorp9k@gmail.com>
2023-01-08 20:56:31 +0200

    Am adugat restul campurile pentru clasa Account

--- Client1/AccountPage.cpp
+++ Client1/AccountPage.cpp
@@ -9,6 +9,10 @@ void AccountPage::displayUsername()
 	ui.usernameEdit->setText(username.c_str());
 	std::string matchesPlayed = AccountRow["matchesPlayed"].s();
 	ui.matchesPlayedEdit->setText(matchesPlayed.c_str());
+	std::string matchesWon = AccountRow["matchesWon"].s();
+	ui.matchesWonEdit->setText(matchesWon.c_str());
+	std::string level = AccountRow["level"].s();
+	ui.levelEdit->setText(level.c_str());
 }
 
 AccountPage::AccountPage(QWidget* parent)
